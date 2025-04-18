9284dfa2239f46015aafd35064d83eb6542cc075
ashwaq-h <shwalyxf@gmail.com>
2025-04-18 13:26:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
-import streamlit as st
-import os
-
-# عنوان الصفحة
-st.title("مترجم النص إلى لغة الإشارة")
-
-# إدخال النص
-text = st.text_input("اكتب الكلمة:")
-
-# عند الضغط على زر الترجمة
 if st.button("ترجم"):
     if text:
         video_path = f"videos/{text}.mp4"
+        st.write("يحاول تشغيل الملف:", video_path)  # <-- أضفنا هذا السطر هنا
+
         if os.path.exists(video_path):
-            st.success(f"ترجمة الكلمة: {text}")
-            st.video(video_path)
+            video_file = open(video_path, 'rb')
+            video_bytes = video_file.read()
+            st.video(video_bytes)
         else:
             st.error("لم يتم العثور على فيديو لهذه الكلمة.")
     else:
